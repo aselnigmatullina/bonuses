353a67b6f84b71c7254ab18c7aad79f92f7db1b5
Student <student@itpark.ru>
2019-09-21 10:56:31 +0300

    Вариант со 1005 возможностью оплаты

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 console.log('worked');
 
+const purchaseAmount=1000;
+const bonuses=900;
+let total;
+if(purchaseAmount<=bonuses){
+   total=0;
+} else{
+   total=purchaseAmount-bonuses;
+}
+console.log(total);
+
+
+
