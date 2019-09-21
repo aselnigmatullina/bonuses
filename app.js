8f850e299bd602be13387d1834862e4a54e7a53f
Student <student@itpark.ru>
2019-09-21 11:25:48 +0300

    50%

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 console.log('worked');
 
-const purchaseAmount=1000;
-const bonuses=900;
+const purchaseAmount = 1000;
+const bonuses = 1000;
 
+const maxBonuses=purchaseAmount*0.5;
+let appliedBonuses;
 
-let total=purchaseAmount-bonuses;
-if(total<0){
-   total=0;
-} 
+if(maxBonuses <= bonuses){
+    appliedBonuses = maxBonuses;
+}else{
+    appliedBonuses = bonuses;
+}
+console.log(appliedBonuses);
+const total = purchaseAmount - appliedBonuses;
 console.log(total);
 
 
